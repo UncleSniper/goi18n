731922018f8166fc8a29316999e0e1b4fda6716a
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2024-04-09 21:57:21 +0200

    Add NumberFormats

--- Localizer.go
+++ Localizer.go
@@ -3,3 +3,45 @@ package goi18n
 type Localizer interface {
 	//TODO
 }
+
+type NumberFormats interface {
+	DecimalSeparator() rune
+	DigitGroupSeparator() rune
+}
+
+type NumberFormatsTemplate struct {
+	DecimalSeparator rune
+	DigitGroupSeparator rune
+}
+
+type numberFormatsImpl struct {
+	decimalSeparator rune
+	digitGroupSeparator rune
+}
+
+func(impl *numberFormatsImpl) DecimalSeparator() rune {
+	return impl.decimalSeparator
+}
+
+func(impl *numberFormatsImpl) DigitGroupSeparator() rune {
+	return impl.digitGroupSeparator
+}
+
+var fallbackNumberFormats *numberFormatsImpl = &numberFormatsImpl {
+	decimalSeparator: '.',
+	digitGroupSeparator: ',',
+}
+
+func ProtectNumberFormats(tpl *NumberFormatsTemplate) NumberFormats {
+	if tpl == nil {
+		return fallbackNumberFormats
+	}
+	return &numberFormatsImpl {
+		decimalSeparator: tpl.DecimalSeparator,
+		digitGroupSeparator: tpl.DigitGroupSeparator,
+	}
+}
+
+func FallbackNumberFormats() NumberFormats {
+	return fallbackNumberFormats
+}
