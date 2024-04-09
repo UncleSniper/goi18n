9609a882404a38309d4def8cad53a46a3578babc
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2024-04-09 21:56:42 +0200

    Add FallbackLocale()

--- Locale.go
+++ Locale.go
@@ -10,3 +10,21 @@ type Locale struct {
 	Variants []Variant
 	Extensions []Extension
 }
+
+func(loc *Locale) SortExtensions() error {
+	//TODO
+	return nil
+}
+
+var fallbackLocale *Locale = &Locale {
+	PrimaryLanguage: PL_English,
+	ExtendedLanguages: nil,
+	Script: SCNM_NONE, //TODO: use Latin
+	Region: RG_NONE, //TODO: use GB
+	Variants: nil,
+	Extensions: nil,
+}
+
+func FallbackLocale() *Locale {
+	return fallbackLocale
+}
